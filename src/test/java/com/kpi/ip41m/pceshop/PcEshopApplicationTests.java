388cebfa0f047974b0410f33a388414b4ff07036
Dmytro Sholom <dmytrosholom@gmail.com>
2015-12-23 06:14:16 +0200

    Setup integration tests

--- src/test/java/com/kpi/ip41m/pceshop/PcEshopApplicationTests.java
+++ src/test/java/com/kpi/ip41m/pceshop/PcEshopApplicationTests.java
@@ -1,18 +1,68 @@
 package com.kpi.ip41m.pceshop;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PcEshopApplication.class)
 @WebAppConfiguration
-public class PcEshopApplicationTests {
+public abstract class PcEshopApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	protected WebApplicationContext wac;
+
+	protected MockMvc mockMvc;
+
+	@Before
+	public void before() {
+		this.mockMvc = webAppContextSetup(this.wac).build();
+	}
+
+	protected MockMvc getMockMvc() {
+		return this.mockMvc;
+	}
+
+	/******************************** http requests ********************************/
+	protected ResultActions get(String collectionResourceUrl) throws Exception {
+		return getMockMvc().perform(MockMvcRequestBuilders.get(collectionResourceUrl));
+	}
+
+	protected <T> ResultActions get(String collectionResourceUrl, T itemResourceId) throws Exception {
+		return get(collectionResourceUrl + "/" + itemResourceId);
+	}
+
+	protected ResultActions post(String collectionresourceUrl, Object object) throws Exception {
+		return getMockMvc().perform(MockMvcRequestBuilders.post(collectionresourceUrl)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(object)));
+	}
+
+	/******************************** helpers ********************************/
+	protected void printResponse(ResultActions resultActions) throws UnsupportedEncodingException {
+		System.out.println(resultActions.andReturn().getResponse().getContentAsString());
+	}
+
+	protected static byte[] asJsonString(Object object) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		return mapper.writeValueAsBytes(object);
 	}
 
 }
